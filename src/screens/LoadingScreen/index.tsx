817e5ed61629d87d50f256bde7769a726cd9e395
Felipe Hernández <feehernandezba@gmail.com>
2023-05-27 19:51:54 -0500

    Fixed title

--- src/screens/LoadingScreen/index.tsx
+++ src/screens/LoadingScreen/index.tsx
@@ -5,9 +5,6 @@ import Head from "next/head";
 function LoadingScreen() {
   return (
     <>
-      <Head>
-        <title>Loading... | {process.env.NEXT_PUBLIC_SITE_NAME}</title>
-      </Head>
       <div className="h-screen w-full bg-gray-800 flex flex-col items-center justify-center">
         <LoadingSpinner />
       </div>
