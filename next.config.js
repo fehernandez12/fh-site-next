dd22ed32f0299beb44a75e1b0e66b213736abe98
Felipe Hernandez <deffeater@Felipes-MacBook-Pro.local>
2024-11-04 22:27:28 -0500

    Headers and CORS configuration for API endpoints.

--- next.config.js
+++ next.config.js
@@ -29,7 +29,8 @@ const nextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "style-src 'self' 'unsafe-inline'",
+            value:
+              "default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com",
           },
         ],
       },
